e1721b1492dff2fb0b1e1f1945cf7fda5a714cb3
MonikaRe <monikareutovaite@gmail.com>
2021-04-27 23:25:54 +0300

    Version 0.1

---
+++ DataProcessing/Methods.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProcessing
+{
+    class Methods
+    {
+        //Method to store input in a list
+        public List<int> List()
+        {
+            List<int> homeworks = new List<int>();
+            string[] marks;
+            int temp;
+            Console.WriteLine("Enter homework marks, separated by space");
+            string input = Console.ReadLine();
+            marks = input.Split(' ');
+            foreach (string i in marks)
+            {
+                temp = int.Parse(i);
+                homeworks.Add(temp);
+            }
+            return homeworks;
+        }
+        //Method to store input in a simple array
+        public int[] SimpleArray()
+        {
+            string[] marks;
+            Console.WriteLine("Enter homework marks, separated by space");
+            string input = Console.ReadLine();
+            marks = input.Split(' ');
+            int n = marks.Length;
+            int[] homeworks = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                homeworks[i] = int.Parse(marks[i]);
+            }
+            return homeworks;
+
+        }
+        //Method to calculate median from a list
+        public double CalculateMedianFromList(List<int> homeworks)
+        {
+            double median = 0;
+            homeworks.Sort();
+            int n = homeworks.Count();
+            if (n % 2 != 0)
+            {
+                median = homeworks[n / 2];
+            }
+            else
+            {
+                median = (homeworks[(n - 1) / 2] + homeworks[n / 2]) / 2.0;
+            }
+            return median;
+        }
+        //Method to calculate median from an array
+        public double CalculateMedianFromArray(int[] homeworks)
+        {
+            double median = 0;
+            Array.Sort(homeworks);
+            int n = homeworks.Length;
+            if (n % 2 != 0)
+            {
+                median = homeworks[n / 2];
+            }
+            else
+            {
+                median = (homeworks[(n - 1) / 2] + homeworks[n / 2]) / 2.0;
+            }
+            return median;
+        }
+
+        //Random number generator
+        public int RandomNumber()
+        {
+            Random r = new Random();
+            int num = r.Next(11);
+            return num;
+        }
+    }
+}
--- DataProcessing/Program.cs
+++ DataProcessing/Program.cs
@@ -10,6 +10,56 @@ namespace DataProcessing
     {
         static void Main(string[] args)
         {
+            string name;
+            string surname;
+            float exam_result = 0;
+            double homeworks_average = 0;
+            double homeworks_median = 0;
+            double final_points = 0;
+            int choice;
+
+            Methods m = new Methods();
+
+            Console.WriteLine("Enter name and surname");
+            string input = Console.ReadLine();
+            string[] c = input.Split(' ');
+            name = c[0];
+            surname = c[1];
+
+            //As per task, student homework results are stored in either array or list. For that, there are different methods. Select which one.
+
+            //With list:
+
+            List<int> homeworks = m.List();
+            //homeworks = m.List();
+            homeworks_average = homeworks.Sum() / homeworks.Count();
+            homeworks_median = m.CalculateMedianFromList(homeworks);
+
+
+            //With Simple array
+            /*int[] homeworks = m.SimpleArray();
+            homeworks_average = homeworks.Sum() / homeworks.Length;
+            homeworks_median = m.CalculateMedianFromArray(homeworks);
+            */
+
+
+            Console.WriteLine("Enter the result of exam");
+            string input2 = Console.ReadLine();
+            float.TryParse(input2, out exam_result);
+
+            Console.WriteLine("Would you like to view final points as average(0) or median(1)");
+            int.TryParse(Console.ReadLine(), out choice);
+            if (choice == 0)
+            {
+                final_points = 0.3 * homeworks_average + 0.7 * exam_result;
+            }
+            else
+            {
+                final_points = 0.3 * homeworks_median + 0.7 * exam_result;
+            }
+            Console.Write(surname + " " + name + " " + final_points);
+
+            Console.ReadLine();
         }
     }
 }
